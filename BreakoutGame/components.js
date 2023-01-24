8f78d2402eb33e573f84c0d82a36f6069c239288
Jonatan Valen <jonatan.valen@student.uib.no>
2023-01-24 17:42:28 +0100

    added images

---
+++ BreakoutGame/components.js
@@ -0,0 +1,3 @@
+
+const BG_IMG = new Image();
+BG_IMG.src = "img/bg.jpg";
--- BreakoutGame/script.js
+++ BreakoutGame/script.js
@@ -1,5 +1,24 @@
-const cvs = document.getElementById("breakout");
-const ctx = cvs.getContext("2d");
+var cvs = document.getElementById("breakout");
+var ctx = cvs.getContext("2d");
 
-ctx.fillStyle = "red";
-ctx.fillRect(0, 0, 100, 100);
+// ADD BORDER TO CANVAS
+cvs.style.border = "1px solid #0ff";
+
+// MAKE LINE THIK WHEN DRAWING TO CANVAS
+ctx.lineWidth = 3;
+
+function drawRect(posX, posY) {
+    ctx.fillStyle = "blue";
+    ctx.fillRect(posX, posY, 50, 50);
+}
+
+function loop(y){
+    //clear the canvas
+    ctx.drawImage(BG_IMG, 0, 0);
+    //draw();
+    //update();
+    requestAnimationFrame(loop);
+
+}
+
+loop(100);
