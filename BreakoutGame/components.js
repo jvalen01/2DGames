5bde4c88f7c9eb18350db13f58b7dc9aaade8ad3
Jonatan Valen <jonatan.valen@student.uib.no>
2023-01-24 18:33:05 +0100

    Made ball and paddle + fixed collision logic

--- BreakoutGame/components.js
+++ BreakoutGame/components.js
@@ -1,3 +1,19 @@
+/////// LOAD IMAGES ////////
 
 const BG_IMG = new Image();
 BG_IMG.src = "img/bg.jpg";
+
+const LEVEL_IMG = new Image();
+LEVEL_IMG.src = "img/level.png";
+
+const LIFE_IMG = new Image();
+LIFE_IMG.src = "img/life.png";
+
+const SCORE_IMG = new Image();
+SCORE_IMG.src = "img/score.png";
+
+
+/////// END LOAD IMAGES ////////
+
+// ************************ //
+
--- BreakoutGame/script.js
+++ BreakoutGame/script.js
@@ -1,24 +1,150 @@
+//Canvas element:
 var cvs = document.getElementById("breakout");
 var ctx = cvs.getContext("2d");
 
 // ADD BORDER TO CANVAS
 cvs.style.border = "1px solid #0ff";
-
 // MAKE LINE THIK WHEN DRAWING TO CANVAS
 ctx.lineWidth = 3;
 
-function drawRect(posX, posY) {
-    ctx.fillStyle = "blue";
-    ctx.fillRect(posX, posY, 50, 50);
+//Variables:
+const PADDLE_WIDTH = 100;
+const PADDLE_MARGIN_BOTTOM = 50;
+const PADDLE_HEIGHT = 20;
+const BALL_RADIUS = 8;
+let leftArrow = false;
+let rightArrow = false;
+let LIFE = 3; // PLAYER HAS 3 LIVES
+
+
+//Make paddle
+const paddle = {
+    x: cvs.width/2 - PADDLE_WIDTH/2,
+    y: cvs.height - PADDLE_MARGIN_BOTTOM - PADDLE_HEIGHT,
+    width: PADDLE_WIDTH,
+    height: PADDLE_HEIGHT,
+    dx: 5
+}
+
+function drawPaddle() {
+    ctx.fillStyle = "#2e3548";
+    ctx.fillRect(paddle.x, paddle.y, paddle.width, paddle.height);
+
+    ctx.strokeStyle = "#ffcd05";
+    ctx.strokeRect(paddle.x, paddle.y, paddle.width, paddle.height);
+}
+
+//Control the paddle
+document.addEventListener("keydown", function(event){
+    if(event.keyCode == 37){
+        leftArrow = true;
+    } else if(event.keyCode == 39){
+        rightArrow = true;
+    }
+});
+
+document.addEventListener("keyup", function(event){
+    if(event.keyCode == 37){
+        leftArrow = false;
+    } else if(event.keyCode == 39){
+        rightArrow = false;
+    }
+});
+
+//Move the paddle
+function movePaddle(){
+    if(rightArrow && paddle.x + paddle.width < cvs.width){
+        paddle.x += paddle.dx;
+    } else if(leftArrow && paddle.x > 0){
+        paddle.x -= paddle.dx;
+    }
+}
+
+//Create ball
+const ball = {
+    x: cvs.width/2,
+    y: paddle.y - BALL_RADIUS,
+    radius: BALL_RADIUS,
+    speed: 4,
+    dx: 3 * (Math.random() * 2 - 1),
+    dy: -3
+}
+
+//Draw ball
+function drawBall(){
+    ctx.beginPath();
+    ctx.arc(ball.x, ball.y, ball.radius, 0, Math.PI*2);
+    ctx.fillStyle = "#ffcd05";
+    ctx.fill();
+    ctx.strokeStyle = "#2e3548";
+    ctx.stroke();
+    ctx.closePath();
+}
+
+//Move ball
+function moveBall(){
+    ball.x += ball.dx;
+    ball.y += ball.dy;
+}
+
+//Ball and wall collision detection
+function ballWallCollision(){
+    if(ball.x + ball.radius > cvs.width || ball.x - ball.radius < 0){
+        ball.dx = -ball.dx;
+    }
+    if(ball.y - ball.radius < 0){
+        ball.dy = -ball.dy;
+    }
+    if(ball.y + ball.radius > cvs.height){
+        LIFE--; // LOSE LIFE
+        resetBall();
+    }
+}
+
+function resetBall(){
+    ball.x = cvs.width/2;
+    ball.y = paddle.y - BALL_RADIUS;
+    ball.dx = 3 * (Math.random() * 2 - 1);
+    ball.dy = -3;
+}
+
+//ball and paddle collision
+function ballPaddleCollision(){
+    if(ball.x < paddle.x + paddle.width && ball.x > paddle.x && ball.y < paddle.y + paddle.height && ball.y > paddle.y){
+        //check where the ball hit the paddle
+        let collidePoint = ball.x - (paddle.x + paddle.width/2);
+        //normalize the values
+        collidePoint = collidePoint / (paddle.width/2);
+        //calculate the angle of the ball
+        let angle = collidePoint * Math.PI/3;
+        ball.dx = ball.speed * Math.sin(angle);
+        ball.dy = -ball.speed * Math.cos(angle);
+    }
+}
+
+
+
+
+
+function draw(){
+    drawPaddle();
+    drawBall();
+}
+
+function update(){
+    movePaddle();
+    moveBall();
+    ballWallCollision();
+    ballPaddleCollision();
 }
 
 function loop(y){
     //clear the canvas
     ctx.drawImage(BG_IMG, 0, 0);
-    //draw();
-    //update();
+    draw();
+    update();
     requestAnimationFrame(loop);
 
 }
 
-loop(100);
+loop();
