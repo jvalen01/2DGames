dab5bf84b7f7f1847a5245d1939c2f1ec87fdcff
Jonatan Valen <jonatan.valen@student.uib.no>
2023-01-25 10:58:11 +0100

    Draw bricks finished

--- BreakoutGame/script.js
+++ BreakoutGame/script.js
@@ -123,12 +123,56 @@ function ballPaddleCollision(){
 }
 
 
+//Create the bricks
+const brick = {
+    row: 3,
+    column: 5,
+    width: 55,
+    height: 20,
+    offSetLeft: 20,
+    offSetTop: 20,
+    marginTop: 40,
+    fillColor: "#2e3548",
+    strokeColor: "#FFF"
+}
+
+function createBricks(){
+    for(let r = 0; r < brick.row; r++){
+        bricks[r] = [];
+        for(let c = 0; c < brick.column; c++){
+            bricks[r][c] = {
+                x: c * (brick.offSetLeft + brick.width) + brick.offSetLeft,
+                y: r * (brick.offSetTop + brick.height) + brick.offSetTop + brick.marginTop,
+                status: true
+            }
+        }
+    }
+}
+
+createBricks();
+
+function drawBricks(){
+    for(let r = 0; r < brick.row; r++){
+        for(let c = 0; c < brick.column; c++){
+            let b = bricks[r][c];
+            //draw the brick only if status is true
+            if(b.status){
+                ctx.fillStyle = brick.fillColor;
+                ctx.fillRect(b.x, b.y, brick.width, brick.height);
+                ctx.strokeStyle = brick.strokeColor;
+                ctx.strokeRect(b.x, b.y, brick.width, brick.height);
+            }
+        }
+    }
+}
+
 
 
 
 function draw(){
     drawPaddle();
     drawBall();
+    drawBricks();
 }
 
 function update(){
