5a46c4a58980535e8b97dc0ff4ddbafa4ab03236
Jonatan Valen <jonatan.valen@student.uib.no>
2023-03-24 15:16:52 +0100

    Added collision logic with paddle, ball and walls

--- Arkanoid/game.js
+++ Arkanoid/game.js
@@ -11,6 +11,8 @@ class gameModel {
         this.PADDLE_WIDTH = 100;
         this.PADDLE_MARGIN_BOTTOM = 50;
         this.PADDLE_HEIGHT = 20;
+        this.BALL_RADIUS = 8;
+        this.LIFE = 3; // The player will have 3 lives.
 
         //Creation of paddle
         this.paddle = { 
@@ -20,6 +22,21 @@ class gameModel {
             height: this.PADDLE_HEIGHT,
             dx: 5
         }
+        //Create ball
+        this.ball = {
+            x: cvs.width/2,
+            y: this.paddle.y - this.BALL_RADIUS,
+            radius: this.BALL_RADIUS,
+            speed: 5,
+            dx: 3 * (Math.random() * 2 - 1),
+            dy: -3
+        }
+    }
+    resetBall(){
+        this.ball.x = cvs.width/2;
+        this.ball.y = this.paddle.y - this.BALL_RADIUS;
+        this.ball.dx = 3 * (Math.random() * 2 - 1);
+        this.ball.dy = -3;
     }
     
 
@@ -34,9 +51,10 @@ class gameModel {
  * Visual representation of the model.
  */
 class gameView {
-    constructor(paddle) {
+    constructor(paddle, ball) {
 
         this.paddle = paddle;
+        this.ball = ball;
         this.rightArrow = false;
         this.leftArrow = false;
 
@@ -64,6 +82,8 @@ class gameView {
         ctx.drawImage(BG_IMG, 0, 0);
 
     }
+
+    //function for drawing the paddle.
     drawPaddle() {
         ctx.fillStyle = "#000000";
         ctx.fillRect(this.paddle.x, this.paddle.y, this.paddle.width, this.paddle.height);
@@ -80,15 +100,35 @@ class gameView {
         }
     }
 
+    //Draw the ball:
+    drawBall(){
+        ctx.beginPath();
+        ctx.arc(this.ball.x, this.ball.y, this.ball.radius, 0, Math.PI*2);
+        ctx.fillStyle = "#000000";
+        ctx.fill();
+        ctx.strokeStyle = "#808080";
+        ctx.stroke();
+        ctx.closePath();
+    }
+
+    //Move ball
+    moveBall(){
+        this.ball.x += this.ball.dx;
+        this.ball.y += this.ball.dy;
+    }
+        
+
 
 
 
     draw(){
         this.drawPaddle();
+        this.drawBall();
     }
 
     update(){
         this.movePaddle();
+        this.moveBall();
 
     }
 
@@ -105,6 +145,7 @@ class gameView {
  * @class Controller
  *
  * Links the user input and the view output.
+ * Handles collision logic.
  *
  * @param model
  * @param view
@@ -114,8 +155,43 @@ class gameController {
         this.model = model;
         this.view = view;
         this.view.paddle = this.model.paddle;
+        this.view.ball = this.model.ball;
     }
 
+    ballWallCollision(){
+        if(this.model.ball.x + this.model.ball.radius > cvs.width || this.model.ball.x - this.model.ball.radius < 0){
+            this.model.ball.dx = -this.model.ball.dx;
+            WALL_HIT.play();
+        }
+        if(this.model.ball.y - this.model.ball.radius < 0){
+            this.model.ball.dy = -this.model.ball.dy;
+            WALL_HIT.play();
+        }
+        if(this.model.ball.y + this.model.ball.radius > cvs.height){
+            this.model.LIFE--; // LOSE LIFE
+            LIFE_LOST.play();
+            this.model.resetBall();
+        }
+    }
+
+    ballPaddleCollision(){
+        if(this.model.ball.x < this.model.paddle.x + this.model.paddle.width && this.model.ball.x > this.model.paddle.x && this.model.ball.y < this.model.paddle.y + this.model.paddle.height && this.model.ball.y > this.model.paddle.y){
+            //check where the ball hit the paddle
+            let collidePoint = this.model.ball.x - (this.model.paddle.x + this.model.paddle.width/2);
+            //normalize the values
+            collidePoint = collidePoint / (this.model.paddle.width/2);
+            //calculate the angle of the ball
+            let angle = collidePoint * Math.PI/3;
+            PADDLE_HIT.play();
+            this.model.ball.dx = this.model.ball.speed * Math.sin(angle);
+            this.model.ball.dy = -this.model.ball.speed * Math.cos(angle);
+        }
+    }
+
+    collisionDetection(){
+        this.ballWallCollision();
+        this.ballPaddleCollision();
+    }
 
 
     loop(){
@@ -123,6 +199,8 @@ class gameController {
         this.view.render();
 
         this.view.update();
+        
+        this.collisionDetection();
         this.view.draw();
 
         
